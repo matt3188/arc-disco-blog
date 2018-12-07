c43eb97fc089cd5ba5fb686eb6150dfc9858dce2
Matt Coleman <matt.coleman@thisisdare.com>
2018-12-07 16:48:37 +0000

    Added feature image to post

--- src/Post.jsx
+++ src/Post.jsx
@@ -3,21 +3,27 @@ import React from "react";
 const Post = ({ post, loading }) => (
   <React.Fragment>
     <div className="container">
-      <div className="row">
-        <div className="col-md-10 offset-md-1 col-lg-8 offset-lg-2">
-          {console.log(post)}
-          {loading ? (
-            "Loading..."
-          ) : (
-            <div>
-              <h1>{post.title.rendered}</h1>
-              <div
-                dangerouslySetInnerHTML={{ __html: post.content.rendered }}
-              />
-            </div>
-          )}
+      {loading ? (
+        <div className="row">
+          <div className="col-md-10 offset-md-1 col-lg-8 offset-lg-2">
+            <p>Loading...</p>
+          </div>
         </div>
-      </div>
+      ) : (
+        <div>
+          <h1>{post.title.rendered}</h1>
+
+          {post.acf.feature_image && (
+            <img
+              src={post.acf.feature_image.url}
+              className="card-img-top mt-4 mb-4"
+              alt=""
+            />
+          )}
+
+          <div dangerouslySetInnerHTML={{ __html: post.content.rendered }} />
+        </div>
+      )}
     </div>
   </React.Fragment>
 );
