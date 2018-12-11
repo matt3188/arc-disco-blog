52430ff810a7edf2f5a2ecbb02f6bf27af191b4f
Matt Coleman <matt.coleman@thisisdare.com>
2018-12-11 11:13:32 +0000

    Updates

---
+++ precache-manifest.4333e82bcfd46a3656bb483b967001be.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9664b7597d1b1162676e",
+    "url": "/static/js/main.9664b759.chunk.js"
+  },
+  {
+    "revision": "65e5a41409e1d38fba95",
+    "url": "/static/js/1.65e5a414.chunk.js"
+  },
+  {
+    "revision": "9664b7597d1b1162676e",
+    "url": "/static/css/main.59d2c666.chunk.css"
+  },
+  {
+    "revision": "65e5a41409e1d38fba95",
+    "url": "/static/css/1.8f17a430.chunk.css"
+  },
+  {
+    "revision": "8214912381902311d5d9443774e2aeee",
+    "url": "/index.html"
+  }
+];
