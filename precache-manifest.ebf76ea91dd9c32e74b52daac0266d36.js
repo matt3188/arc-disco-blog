04f2bce8e8f23a418d21ab148258d321c54e5972
Matt Coleman <matt.coleman@thisisdare.com>
2018-12-11 11:20:38 +0000

    Updates

---
+++ precache-manifest.ebf76ea91dd9c32e74b52daac0266d36.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f319367b9b9fc1f136fc",
+    "url": "/static/js/main.f319367b.chunk.js"
+  },
+  {
+    "revision": "65e5a41409e1d38fba95",
+    "url": "/static/js/1.65e5a414.chunk.js"
+  },
+  {
+    "revision": "f319367b9b9fc1f136fc",
+    "url": "/static/css/main.59d2c666.chunk.css"
+  },
+  {
+    "revision": "65e5a41409e1d38fba95",
+    "url": "/static/css/1.8f17a430.chunk.css"
+  },
+  {
+    "revision": "5afacf0fbfdbc20053d867e45ef766e9",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/arc-disco-blog/precache-manifest.864ce83c5b0873945ff1f0a4f4a8a746.js"
+  "/precache-manifest.ebf76ea91dd9c32e74b52daac0266d36.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/arc-disco-blog/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
